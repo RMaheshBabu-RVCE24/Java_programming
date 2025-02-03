c798c5d03f17aa25cfa7f8985c1c77bccc3418bc
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-03 19:02:37 +0530

    Logical_AND(Basic - Problems).java

---
+++ Logical_AND(Basic - Problems).java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        String citizen = "Indian";
+        if (age >= 18 & citizen == "Indian") {
+            System.out.println("Person can vote");
+        } else {
+            System.out.println("Person cannot vote");
+        }
+    }
+    }
