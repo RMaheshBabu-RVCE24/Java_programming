29864c3dc128ded919fb5a0be2c1957882ab525a
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-03 19:18:39 +0530

    Logical_OR( | )(Basic_Programs).java

---
+++ Logical_OR( | )(Basic_Programs).java
@@ -0,0 +1,14 @@
+/* problem-1: if person belongs to the organization
+  Condition : Organization = Mindtech
+*/
+public class Main {
+    public static void main(String[] args) {
+       String Organization="Mindtech";
+       if(Organization=="Mindtech" | Organization=="Different"){
+           System.out.println("Employee");
+       }
+       else{
+           System.out.println("Not an Employee");
+       }
+    }
+    }
