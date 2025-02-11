e4161e38e8685512de43195968a0deb0c01d2190
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-11 22:40:42 +0530

    For Loop Basic Programs.java

---
+++ For Loop Basic Programs.java
@@ -0,0 +1,15 @@
+/* print 1 to 10 using for for loop */
+public class Main {
+    public static void main(String[] args) {
+       for(int i=1;i<=10;i++){
+           System.out.print(i);
+       }
+    }}
+
+/* print i 10 times using for for loop */
+public class Main {
+    public static void main(String[] args) {
+       for(int i=1;i<=10;i++){
+           System.out.print(" "+"i");
+       }
+    }}
