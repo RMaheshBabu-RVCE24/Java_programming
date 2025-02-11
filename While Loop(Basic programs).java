dc2bb0260f7844f5de670ad0aac22e0dddef1eb8
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-11 22:46:13 +0530

    While Loop(Basic programs).java

--- While Loop(Basic programs).java
+++ While Loop(Basic programs).java
@@ -1,1 +1,9 @@
-
+/* print 1 to 10 using for for loop */
+public class Main {
+    public static void main(String[] args) {
+       int a=1:
+       while(a<=10){
+           System.out.println(a);
+          i++;
+       }
+    }}
