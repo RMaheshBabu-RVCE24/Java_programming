42a1e7f6d6ced74e8bc4c974bf9a3c3c4baec36b
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-26 18:02:28 +0530

    tables of any given input no.java

---
+++ tables of any given input no.java
@@ -0,0 +1,13 @@
+public class Main {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int sum=0;
+            int num=sc.nextInt();
+            for(int i=1;i<=num;i++){
+                System.out.print(i);
+                sum=sum+num;
+                System.out.println(" "+sum);
+
+            }
+            }
+    }
